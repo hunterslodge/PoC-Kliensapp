30c991a6c57d25f94c139c0b6cb06b4732d0fa26
UWRH74 <attila.gyurko@stud.uni-corvinus.hu>
2023-05-01 19:16:21 +0200

    test4

--- UnitTestProject2/Unittest.cs
+++ UnitTestProject2/Unittest.cs
@@ -23,55 +23,25 @@ namespace UnitTestProject2
     public class Unittest
     {
         [Test]
-        public void TestGetProducts()
+        public void TestDeleteProduct()
         {
             // Arrange
-            DataTable expectedTable = new DataTable();
-            expectedTable.Columns.Add("Bvin", typeof(string));
-            expectedTable.Columns.Add("Sku", typeof(string));
-            expectedTable.Columns.Add("ProductName", typeof(string));
-            expectedTable.Columns.Add("SitePrice", typeof(string));
-            expectedTable.Columns.Add("CreationDateUtc", typeof(DateTime));
-            DataRow row1 = expectedTable.NewRow();
-            row1["Bvin"] = "123";
-            row1["Sku"] = "ABC";
-            row1["ProductName"] = "Product1";
-            row1["SitePrice"] = "10 Ft";
-            row1["CreationDateUtc"] = DateTime.Now;
-            expectedTable.Rows.Add(row1);
-            ApiResponse<List<ProductDTO>> response = new ApiResponse<List<ProductDTO>>();
-            List<ProductDTO> content = new List<ProductDTO>();
-            ProductDTO product = new ProductDTO();
-            product.Bvin = "123";
-            product.Sku = "ABC";
-            product.ProductName = "Product1";
-            product.SitePrice = 10;
-            product.CreationDateUtc = DateTime.Now;
-            content.Add(product);
-            response.Content = content;
-            string json = JsonConvert.SerializeObject(response);
-            string url = "http://20.234.113.211:8095/";
-            string key = "1-be27b88a-de65-48f3-9d66-fea7e3179d36";
-            Mock<Api> mockApi = new Mock<Api>(url, key);
-            mockApi.Setup(m => m.ProductsFindAll()).Returns(response);
-            Form1 form = new Form1();
+            var form = new Form1();
             form.productDataGridView = new DataGridView();
+            var expectedRowCount = form.productDataGridView.Rows.Count - 1;
+            // add a row to DataGridView to delete
+            form.productDataGridView.Rows.Add("123", "ABC", "Product1", "10 Ft", DateTime.Now);
 
             // Act
-            form.GetProducts();
-            DataTable actualTable = (DataTable)form.productDataGridView.DataSource;
+            var scrollPosition = form.productDataGridView.FirstDisplayedScrollingRowIndex;
+            form.Törlés_fv();
+            var actualRowCount = form.productDataGridView.Rows.Count;
+            var lastRowIndex = actualRowCount - 1;
+            var actualProductId = lastRowIndex >= 0 ? form.productDataGridView.Rows[lastRowIndex].Cells[0].Value : null;
 
             // Assert
-            Assert.AreEqual(expectedTable.Rows.Count, actualTable.Rows.Count);
-            Assert.AreEqual(expectedTable.Columns.Count, actualTable.Columns.Count);
-            for (int i = 0; i < expectedTable.Rows.Count; i++)
-            {
-                for (int j = 0; j < expectedTable.Columns.Count; j++)
-                {
-                    Assert.AreEqual(expectedTable.Rows[i][j], actualTable.Rows[i][j]);
-                }
-            }
+            Assert.AreEqual(expectedRowCount, actualRowCount);
+            Assert.IsNull(actualProductId);
         }
-        
     }
 }
